132a53f3e37cda34cccc0fbbac9d7637797a76a9
LES8638 <105543590+LES8638@users.noreply.github.com>
2025-08-30 08:57:03 +0900

    fix: cluster-member/me request,response body 수정

--- api/routes.py
+++ api/routes.py
@@ -17,7 +17,6 @@ from pydantic import BaseModel
 from fastapi import Body
 
 class ClusterRequest(BaseModel):
-    groupId: int  # 일단 받기만 함 (미사용)
     userId: int
     topK: int = 5   # 기본값 5, 유효범위는 1~100으로 검증할 수도 있음
 
@@ -25,7 +24,6 @@ class ClusterRequest(BaseModel):
 def my_cluster_post(payload: ClusterRequest = Body(...)):
     campus_id = settings.CAMPUS_ID
     user_id = payload.userId
-    _group_id = payload.groupId  # 나중에 사용 예정(현재 미사용)
     top_k = payload.topK
 
     if not (1 <= top_k <= 100):
@@ -58,7 +56,4 @@ def my_cluster_post(payload: ClusterRequest = Body(...)):
     if top_k:
         members = members[:top_k]
 
-    return {
-            "groupId": _group_id,
-            "userIds": [uid for uid in members],
-    }
+    return [uid for uid in members]
